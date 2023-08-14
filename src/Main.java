dd02cd1f6102c675851d344c69e0f6b67737de55
RobertAzatovichSitdikov <kotiknumbertwo@gmail.com>
2023-08-14 18:26:52 +0300

    а

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,10 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
+        // no clone
         System.out.println("Я так и не понял, как это вообще работает");
+
+        System.out.println("ajara guju");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
